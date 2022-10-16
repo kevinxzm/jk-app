6340dbdba28ff3151d5a410a93d5ed7e78a44487
kevinxzm <kevinxuzhimeng@gmail.com>
2022-10-16 21:26:54 +1100

    change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import './App.scss';
+import Home from "./pages/Home/Home"
+import Login from "./pages/Login/Login"
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">1235656
-      
+    <div className="App">
+
+
+
+      <Home></Home>
+      <Login></Login>
     </div>
   );
 }
